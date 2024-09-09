4d4b8402ebe10b32c5a8b9e68fa7e849b2d1825d
Chris Smiley <csmiley@ufl.edu>
2024-09-09 17:27:36 -0400

    Setup react and tailwinds

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import "../../css/app.css"
+export default function Home() {
+    return  (
+        <>
+            <h1 className="font-semibold, text-orange-600">Hello World</h1>
+        </>
+    )
+}
