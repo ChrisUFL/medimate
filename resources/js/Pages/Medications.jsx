65b9920e4099f60047c76e1f783526059e2129d7
Chris Smiley <csmiley@ufl.edu>
2024-09-13 17:56:22 -0400

    Add simple navbar and routes

---
+++ resources/js/Pages/Medications.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const Medications = () => {
+  return (
+    <div>
+        <Navbar />
+        Medications</div>
+  )
+}
+
+export default Medications
