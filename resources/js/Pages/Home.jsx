8ef1e845fcd8bfead9ca4742f057a61a9c82437f
Chris Smiley <csmiley@ufl.edu>
2024-09-14 07:42:27 -0400

    Add simple home page cards

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,11 +4,21 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import HomepageTile from "../Components/HomepageTile"
 
-const Home = ({ user }) => {
+const Home = ({ user, calendar }) => {
   return (
-    <div className={'w-[1280px] m-auto'}>
-        <Navbar />
-        <HomepageTile user={ user }/>
+    <div className="wrapper flex">
+      <div className={'w-[1280px] m-auto'}>
+          <Navbar />
+          <div className="column flex">
+            <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5 mb-5"/>
+            <HomepageTile title={ "Calendar" } data={ calendar }/>
+          </div>
+          <div className="column flex">
+            <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5 mb-5"/>
+            <HomepageTile title={ "Calendar" } data={ calendar }/>
+          </div>
+          
+      </div>
     </div>
   )
 }
