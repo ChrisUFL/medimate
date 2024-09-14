adfd6a6cd3999b57759dca4fe372ee7917ce88f4
Chris Smiley <csmiley@ufl.edu>
2024-09-14 09:28:17 -0400

    Update navbar styling

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -5,7 +5,8 @@ import logo from '../../../public/static/images/logo_64.png'
 
 const Navbar = () => {
   return (
-    <div className='flex align-middle justify-between mt-5 mb-5 font-medium text-center text-l leading-8'>
+    <div className='mb-5 font-medium text-center text-l leading-8 py-7 bg-white w-screen'>
+      <div className="content flex align-middle justify-between px-10 max-w-[1280px] m-auto">
         <div className='logo flex'>
           <Link href={ route('web.home') }>
           <img src='../../static/images/logo_64.png' alt={ logo } width={ 32 } height={ 32 }/>
@@ -24,6 +25,7 @@ const Navbar = () => {
         <div className="account">
           <Link href={''}>Login</Link>
         </div>
+    </div>
     </div>
   )
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -6,18 +6,19 @@ import HomepageTile from "../Components/HomepageTile"
 
 const Home = ({ user, calendar }) => {
   return (
-    <div className="wrapper flex">
+    <div className="wrapper flex-col bg-gray-100 min-h-screen">
+      <Navbar />
       <div className={'w-[1280px] m-auto'}>
-          <Navbar />
-          <div className="column flex">
-            <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5 mb-5"/>
-            <HomepageTile title={ "Calendar" } data={ calendar }/>
-          </div>
-          <div className="column flex">
-            <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5 mb-5"/>
-            <HomepageTile title={ "Calendar" } data={ calendar }/>
-          </div>
-          
+          <section className="context p-4">
+            <div className="column flex">
+              <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5 mb-5"/>
+              <HomepageTile title={ "Calendar" } data={ calendar }/>
+            </div>
+            <div className="column flex">
+              <HomepageTile title={ "Calendar" } data={ calendar } styles="mr-5"/>
+              <HomepageTile title={ "Calendar" } data={ calendar }/>
+            </div>
+          </section>
       </div>
     </div>
   )
