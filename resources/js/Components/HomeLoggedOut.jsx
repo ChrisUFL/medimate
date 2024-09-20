2b95d6eb2331d7afc66a9b5b0de2d3ae8a6e2431
Chris Smiley <csmiley@ufl.edu>
2024-09-19 21:35:21 -0400

    Add homepage tiles logged out

--- resources/js/Components/HomeLoggedOut.jsx
+++ resources/js/Components/HomeLoggedOut.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HomepageLoggedOutTile from './HomepageLoggedOutTile'
 
 const HomeLoggedOut = () => {
   return (
@@ -6,6 +7,12 @@ const HomeLoggedOut = () => {
         <div className='title flex-col m-auto justify-center'>
             <h1 className='header text-slate-800 font-semibold text-7xl'>Medimate</h1>
             <h3 className='footer text-slate-800 font-normal text-xl mt-1'>Simplifying the Healthcare Process</h3>
+        </div>
+
+        <div className='flex mt-[120px] justify-center'>
+            <HomepageLoggedOutTile color="bg-gray-300"/>
+            <HomepageLoggedOutTile color="bg-gray-400"/>
+            <HomepageLoggedOutTile color="bg-gray-500"/>
         </div>
     </section>
   )
