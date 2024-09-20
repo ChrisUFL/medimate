b3dd8066de1f47eea7cfa519b328be99db64e80d
Chris Smiley <csmiley@ufl.edu>
2024-09-19 21:44:29 -0400

    Fix bg image

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -34,7 +34,7 @@ function IsSignedIn({ user, calendar }) {
 const Home = ({ user, calendar }) => {
   return (
     <div className="wrapper flex-col">
-      <img src={ Homehead } alt="" className="-z-10 absolute min-h-screen object-center bg-no-repeat object-cover blur-sm"/>
+      <img src={ Homehead } alt="" className="-z-10 absolute mx-auto w-[3000px] min-h-screen left-0 right-0 bg-no-repeat object-cover blur-sm"/>
       <Navbar />
       <div className={'w-[1280px] m-auto z-10'}>
           <section className="content p-4">
