79da7a230791a56ae3ccbca091484be373de06fd
KJ O'BRIEN <kjokjo219@gmail.com>
2024-10-04 16:54:39 -0400

    working test passing attributes to component

--- resources/js/Components/Contact.jsx
+++ resources/js/Components/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => {
+    console.log(props)
     return (
         <tr>
-            <td class="border border-gray-400 px-3 py-2">John Smith</td>
+            <td class="border border-gray-400 px-3 py-2">{props.name}</td>
             <td class="border border-gray-400 px-3 py-2">j.smith@gmail.com</td>
             <td class="border border-gray-400 px-3 py-2">+1(555) 555-5555</td>
             <td class="border border-gray-400 px-3 py-2">123 Smith Street</td>
--- resources/js/Pages/AddressBook.jsx
+++ resources/js/Pages/AddressBook.jsx
@@ -3,34 +3,34 @@ import Navbar from '../Components/Navbar'
 import Contact from '../Components/Contact';
 import "../../css/app.css"
 
+const data = {
+    name : "KJ OBrien",
+    email : "kj@gmail.com",
+    phone: "+1 (561) 555-5555",
+    address : "123 Main Street"
+
+}
+
 
 const AddressBook = () => {
     return (
         <div>
             <Navbar />
             <div class = "flex justify-center items-center flex-col">
-
                 <input class = "border border-solid rounded-md w-4/5 p-4 m-4" type="text" placeholder = "Search..."/>
-                <div class = "w-4/5">
-                    <table class="table-auto w-full border rounded-bg border-gray-400">
-                        <tr class="bg-blue-700 text-white rounded-sm">
-                            <th class="border border-gray-400 px-3 py-2">Name</th>
-                            <th class="border border-gray-400 px-3 py-2">Email</th>
-                            <th class="border border-gray-400 px-3 py-2">Phone Number</th>
-                            <th class="border border-gray-400 px-3 py-2">Address</th>
-                        </tr>
-                        <Contact></Contact>
-                        <Contact></Contact>
-                        <Contact></Contact>
-                        <Contact></Contact>
-                        
-                    </table>
-                    
-                   
-                </div>
-                
+                <table class="w-4/5 table-auto border rounded-lg  border-gray-400">
+                    <tr class="bg-blue-700 text-white border rounded-lg">
+                        <th class="border border-gray-400 px-3 py-2">Name</th>
+                        <th class="border border-gray-400 px-3 py-2">Email</th>
+                        <th class="border border-gray-400 px-3 py-2">Phone Number</th>
+                        <th class="border border-gray-400 px-3 py-2">Address</th>
+                    </tr>
+                    <Contact name={data.name}></Contact>
+                    <Contact></Contact>
+                    <Contact></Contact>
+                    <Contact></Contact>
+                </table>
             </div>
-            
         </div>
     );
 };
