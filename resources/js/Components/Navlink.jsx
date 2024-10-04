8e6c8ff736bb1fd1fbf54e71c5ee3a73deea7738
KellerBonnett <73551672+KellerBonnett@users.noreply.github.com>
2024-10-04 14:28:07 -0400

    Update Navlink.jsx

--- resources/js/Components/Navlink.jsx
+++ resources/js/Components/Navlink.jsx
@@ -1,16 +1,18 @@
-import { Link } from '@inertiajs/react'
-import React from 'react'
+import { Link } from '@inertiajs/react';
 
-const Navlink = ({name, routeName}) => {
-    let currentRouteStyle = '';
-
-    if (route().current(routeName)) {
-        currentRouteStyle = 'text-slate-500'
-    }
-
+export default function NavLink({ active = false, className = '', children, ...props }) {
     return (
-      <Link href={route(`${routeName}`)} className={currentRouteStyle}>{name}</Link>
-    )
+        <Link
+            {...props}
+            className={
+                'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
+                (active
+                    ? 'border-indigo-400 focus:border-indigo-700 '
+                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300') +
+                className
+            }
+        >
+            {children}
+        </Link>
+    );
 }
-
-export default Navlink
