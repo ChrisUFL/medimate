32d75a947ec3eb6657956f14113aad5c4658ba80
Chris Smiley <csmiley@ufl.edu>
2024-10-11 09:26:35 -0400

    Add full calendar

--- resources/js/Pages/Provider/Home.jsx
+++ resources/js/Pages/Provider/Home.jsx
@@ -1,49 +1,14 @@
 import React from 'react'
 import ProviderLayout from '../../Layouts/ProviderLayout'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 function Home() {
   return (
     <ProviderLayout pageTitle={'Provider'}>
-      <div className='w-full'>
-        <table className='w-full text-md text-left'>
-        <thead className='text-xs uppercase bg-gray-700 text-gray-400'>
-          <tr>
-            <th className='px-6 py-3'>Appt. Time</th>
-            <th className='px-6 py-3'>Name</th>
-            <th className='px-6 py-3'>View Patient</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='even:bg-indigo-100'>
-            <td className='px-6 py-2'>11:30</td>
-            <td className='px-6 py-2'>Chris Smiley</td>
-            <td className='px-6 py-2'>Link</td>
-          </tr>
-          <tr className='even:bg-indigo-100'>
-            <td className='px-6 py-3'>11:30</td>
-            <td className='px-6 py-3'>Chris Smiley</td>
-            <td className='px-6 py-3'>Link</td>
-          </tr>
-          <tr className='even:bg-indigo-100'>
-            <td className='px-6 py-3'>11:30</td>
-            <td className='px-6 py-3'>Chris Smiley</td>
-            <td className='px-6 py-3'>Link</td>
-          </tr>
-          <tr className='even:bg-indigo-100'>
-            <td className='px-6 py-3'>11:30</td>
-            <td className='px-6 py-3'>Chris Smiley</td>
-            <td className='px-6 py-3'>Link</td>
-          </tr>
-          <tr className='even:bg-indigo-100'>
-            <td className='px-6 py-3'>11:30</td>
-            <td className='px-6 py-3'>Chris Smiley</td>
-            <td className='px-6 py-3'>Link</td>
-          </tr>
-
-        </tbody>
-      </table>
-      </div>
-      
+      <FullCalendar 
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"/>
     </ProviderLayout>
   )
 }
