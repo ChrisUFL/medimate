6d1e82ae5351c554a5a969df6f98e12f9e61af91
KJ O'BRIEN <kjokjo219@gmail.com>
2024-10-19 15:57:36 -0400

    adds ag grid, currently has both tables will convert

--- resources/js/Pages/AddressBook.jsx
+++ resources/js/Pages/AddressBook.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from '../Components/Navbar'
 import Contact from '../Components/Contact';
+import { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-balham.css'; 
 import "../../css/app.css"
 
 const data = [
@@ -26,9 +30,19 @@ const data = [
 
 
 const AddressBook = ({contacts}) => {
+    const [rowData] = useState(data);
+    
+    const [columnDefs] = useState([
+        { field: 'name', headerName: 'Name', flex: 1 },
+        { field: 'email', headerName: 'Email', flex: 1 },
+        { field: 'phone', headerName: 'Phone', flex: 1},
+        { field: 'address', headerName: 'Address', flex: 1 }
+      ]);
     console.log(contacts);
     return (
+
         <div>
+
             <Navbar />
             <div class = "flex justify-center items-center flex-col">
                 <input class = "border border-solid rounded-md w-4/5 p-4 m-4" type="text" placeholder = "Search..."/>
@@ -48,6 +62,19 @@ const AddressBook = ({contacts}) => {
 
                 </table>
             </div>
+            <div>
+            <div class = "flex flex-col h-screen"className="ag-theme-balham" style={{ width: '80%', margin: '20px auto' }}>
+            <AgGridReact 
+            rowData={rowData}  
+            columnDefs={columnDefs}
+            pagination={true} 
+            domLayout="autoHeight"
+        
+            />
+            </div>
+            
+            </div>
+
         </div>
     );
 };
