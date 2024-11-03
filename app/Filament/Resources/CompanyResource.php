093a0e5a474dc08bf7500693e41a42928131aa51
Chris Smiley <csmiley@ufl.edu>
2024-11-03 10:20:32 -0500

    Add company and employee pages

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Models\Company;
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')
+                    ->label('Owner')
+                    ->relationship('owner', 'email')
+                    ->required(),
+                TextInput::make('company_name')
+                    ->label('Company Name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('street_address')
+                    ->label('Street Address')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('city')
+                    ->label('City')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('state')
+                    ->label('State')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('zip_code')
+                    ->label('Zip Code')
+                    ->required()
+                    ->maxLength(255),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('company_name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('user_id'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'create' => Pages\CreateCompany::route('/create'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+}
