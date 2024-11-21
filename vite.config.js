c96aaaaae0052ff47d5ab633d868f952a156bd3f
KJ O'BRIEN <kjokjo219@gmail.com>
2024-11-20 19:31:08 -0500

    working api call

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd()); // Load environment variables based on mode
+
+    console.log('Loaded env variables:', env); // Debug log to verify variables are loaded
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        define: {
+            'process.env': { ...env }, // Pass all loaded env variables to process.env
+        }
+    };
 });
