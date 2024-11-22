5933a8eea7f261bde04acd38eaea54ee7868b2f6
Chris Smiley <csmiley@ufl.edu>
2024-11-22 07:12:03 -0500

    Add redirect to address book store

--- app/Http/Controllers/AddressBookController.php
+++ app/Http/Controllers/AddressBookController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-use App\Models\Contact;
 
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AddressBookController extends Controller
@@ -13,13 +13,15 @@ class AddressBookController extends Controller
     public function index()
     {
         $contacts = Contact::where('user_pk', Auth::id())->get();
+
         return Inertia::render('AddressBook', [
             'contacts' => $contacts,
         ]);
     }
-    
+
     //Store new value contact into the databsae
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
@@ -28,24 +30,25 @@ class AddressBookController extends Controller
         ]);
         $validatedData['user_pk'] = Auth::id();
         Contact::create($validatedData);
+
+        return redirect(route('web.addressbook'));
     }
 
     //Delete a contact from the databsae
     public function delete(Request $request)
     {
-        $deletedRows = Contact::where('user_pk', Auth::id())
-                              ->where('name', $request->name)
-                              ->where('email', $request->email)
-                              ->where('address', $request->address)
-                              ->where('phone', $request->phone)
-                              ->delete();
-    
+        $deletedRows = Contact::query()
+            ->where('user_pk', Auth::id())
+            ->where('name', $request->name)
+            ->where('email', $request->email)
+            ->where('address', $request->address)
+            ->where('phone', $request->phone)
+            ->delete();
+
         if ($deletedRows) {
             return response()->json(['message' => 'deleted successfully']);
         } else {
             return response()->json(['message' => 'did not delete']);
         }
     }
-    
-
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'user_pk',
         'name',
